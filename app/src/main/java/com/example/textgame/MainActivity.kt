bed1d85e22f542d51cb95c44dc7a8a4d6670718d
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-01 16:08:13 -0500

    Messing with modifiers

--- app/src/main/java/com/example/textgame/MainActivity.kt
+++ app/src/main/java/com/example/textgame/MainActivity.kt
@@ -4,10 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.NavigationRailDefaults
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.textgame.ui.theme.TextGameTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,17 +23,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Green),
+                    //.fillMaxSize()
+                    .fillMaxHeight(.7f)
+                    .fillMaxWidth(.7f)
+                    //.width(500.dp)
+                    //.height(500.dp)
+                    .background(Color.Yellow),
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Bottom
+                verticalArrangement = Arrangement.SpaceEvenly
             ){
                 Text( "Testing")
                 Text("Bacon")
+                Text("Hello again!")
             }
-            }
+
         }
     }
+}
 
 @Composable
 fun Greeting(name: String) {
